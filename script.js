a71490c649a8bc93821838767987aac64a782936
Felipe <Felipe.delcampo.williams@gmail.com>
2024-07-29 13:30:37 -0400

    arreglosobjetos

---
+++ script.js
@@ -0,0 +1,57 @@
+/* Arreglos */
+let arreglo=[1,2,3,4,5,6,7,8,9];
+console.log((arreglo.length))
+let variable = `esto es una prueba`
+console.log(variable[3])
+
+arreglo[0]=`primera cadena`
+arreglo[1]=`segunda cadena`
+arreglo[2]=`tercera cadena`
+
+console.log(arreglo[2])
+
+/* Objetos Literales */
+let persona = {
+    rut:`11111111-1`,
+    nombre: `felipe`,
+    fecha: `01/01/1990`
+
+}
+console.log(persona)
+console.log(persona.rut)
+console.log(persona.nombre)
+console.log(persona.fecha)
+
+let curso = {
+    rut:`11111111-1`,
+    nombre: `felipe`,
+    fecha: `01/01/1990`,
+    direccion: {
+        calle: 'cost',
+        numero: `6722`,
+        comuna: `Cerro navia`
+    }
+}
+
+console.log(curso)
+console.log(curso.direccion.calle)
+
+/* Arreglos con objetos */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scriptArregloObjetos.js
@@ -0,0 +1,17 @@
+let clientes = [
+    {
+        rut: '1111111-1',
+        nombre: `Felipe`,
+        fechanac: `01-01-1991`
+    },
+    {
+        rut: '2222222-2',
+        nombre: `Antonio`,
+        fechanac: `02-02-1992`
+    }
+
+]
+
+console.log(clientes)
+console.log(clientes[0].rut)
+console.log(clientes[1].nombre)
